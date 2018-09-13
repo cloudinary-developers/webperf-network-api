177605b1c858203d252a8943a8f8da876a6e42c7
Tamas Piros <tpiros@tpiros-macbook.local>
2018-09-13 20:23:58 +0200

    init

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 app.use(express.static(`${__dirname}/`));
 
 const server = app.listen(8081, () => {
+  console.log(server);
   const host = server.address().address;
   const port = server.address().port;
   console.log(`App listening at http://${host}:${port}`);
